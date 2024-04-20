ff0744cad65587d896c7a5fdc1460a90d1f9510b
Staxed <staxed@aeonforge.io>
2024-04-20 13:46:48 -0400

    add main content component

---
+++ af-web/components/MainContent.tsx
@@ -0,0 +1,9 @@
+import classes from '/styles/MainContent.module.css';
+
+export function maincontent() {
+  return (
+    <img src="AF.png" height="1500px"></img>
+  );
+}
+
+export default maincontent;
--- af-web/pages/index.tsx
+++ af-web/pages/index.tsx
@@ -2,13 +2,14 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
 import HeaderMenu from '../components/HeaderMenu';
 import FooterMenu from '../components/FooterMenu';
+import MainContent from '../components/MainContent';
 import { AppShell } from '@mantine/core';
 
 const Home: NextPage = () => {
   return (
-    <AppShell withBorder={false}>
+    <AppShell padding="0px" withBorder={false}>
       <AppShell.Header><HeaderMenu /></AppShell.Header>
-      <AppShell.Main></AppShell.Main>
+      <AppShell.Main><MainContent /></AppShell.Main>
       <AppShell.Footer><FooterMenu /></AppShell.Footer>
     </AppShell>
   );
