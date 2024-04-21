ecdc442ed808ec3b2825287f1242eb224aeaf91b
Staxed <staxed@aeonforge.io>
2024-04-20 21:21:20 -0400

    update charity page to show main content

--- af-web/pages/charity.tsx
+++ af-web/pages/charity.tsx
@@ -2,13 +2,16 @@ import type { NextPage } from 'next';
 import HeaderMenu from '../components/HeaderMenu';
 import FooterMenu from '../components/FooterMenu';
 import { AppShell } from '@mantine/core';
+import classes from '/styles/Home.module.css';
 
 const Charity: NextPage = () => {
   return (
     <AppShell padding="0px" withBorder={false}>
       <AppShell.Header><HeaderMenu /></AppShell.Header>
       <AppShell.Main>
-        Charity Stuff Here
+        <main className={classes.main}>
+          Charity Stuff Here
+        </main>
       </AppShell.Main>
       <AppShell.Footer><FooterMenu /></AppShell.Footer>
     </AppShell>
