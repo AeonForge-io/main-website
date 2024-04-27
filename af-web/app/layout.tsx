2530b50b148d6a02d406cc8bc0a9c36f278e8b91
Staxed <staxed@aeonforge.io>
2024-04-26 21:33:08 -0400

    remove duplicate html/header/body tags and metadata

--- af-web/app/layout.tsx
+++ af-web/app/layout.tsx
@@ -7,18 +7,10 @@ import Footer from '../components/footer';
 
 export default function RootLayout({ children }: { children: any }) {
   return (
-    <html lang="en">
-      <head>
-        <ColorSchemeScript />
-        <link rel="shortcut icon" href="/favicon.svg" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-        />
-      </head>
-      <body>
-        <Providers><Header />{children}<Footer /></Providers>
-      </body>
-    </html>
+    <Providers>
+      <Header />
+      {children}
+      <Footer />
+    </Providers>
   );
 }
--- af-web/app/providers.tsx
+++ af-web/app/providers.tsx
@@ -52,11 +52,6 @@ export function Providers({ children }: { children: any }) {
     <html lang="en">
       <head>
         <ColorSchemeScript />
-        <link rel="shortcut icon" href="/favicon.svg" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-        />
       </head>
       <body>
         <MantineProvider defaultColorScheme='dark'>
